cf2371abbf8dec92edd019140c8bc3d84e2b31f3
Josh Curtiss <josh@curtiss.me>
2026-06-05 00:55:43 -0500

    test: Update tests for refactor

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,15 +1,15 @@
 import { describe, it, expect } from 'vitest';
+import './mocks/video-js-mock';
+import videojs from '@silvermine/video.js';
 import initializePlugin from '../src/js';
-import { mockVideoJs } from './mocks/video-js-mock';
 
 describe('Remote Playback Plugin', () => {
    it('should initialize plugin without errors', () => {
       const initFunction = (): void => {
-         initializePlugin(mockVideoJs);
+         initializePlugin(videojs);
       };
 
       expect(initFunction).not.toThrow();
-
-      expect(mockVideoJs.registerPlugin).toHaveBeenCalled();
+      expect(videojs.registerPlugin).toHaveBeenCalled();
    });
 });
