66cd51a3d5c6fd76db264ab90085a8c6f644b35e
Josh Curtiss <josh@curtiss.me>
2026-06-05 00:55:29 -0500

    test: Update example to work with refactored code
    In addition to updating the example to work with the refactored code, we also add a test
    dispose button to the example. This allows users to easily test the dispose functionality
    of the code.

--- examples/main.ts
+++ examples/main.ts
@@ -2,14 +2,25 @@ import videojs from '@silvermine/video.js';
 import initializePlugin from '../src/js/index';
 import { isPlayerWithRemotePlaybackPlugin } from '../src/js/RemotePlaybackPlugin';
 import '@silvermine/video.js/dist/video-js.css';
+import '../src/styles/index.scss';
 
 initializePlugin(videojs);
 
 
-const player = videojs('airplay-test-player');
+const player = videojs('remoteplayback-test-player'),
+      disposePlayerButton = document.getElementById('dispose-player-btn');
 
 if (isPlayerWithRemotePlaybackPlugin(player)) {
    player.remotePlayback();
 } else {
    videojs.log.error('Failed to startup Remote Playback plugin.');
 }
+
+function disposeTestPlayer(): void {
+   if (!player || player.isDisposed()) {
+      return;
+   }
+   player.dispose();
+}
+
+disposePlayerButton?.addEventListener('click', disposeTestPlayer);
