2332ad6d1bf7e65e2aff0cb4ce3ae788e1320524
Josh Curtiss <josh@curtiss.me>
2026-06-11 22:52:41 -0500

    test: Update mock classes with more methods
    - Update MockButton to include `player()`
    - Update MockPlugin to include `player` property and `dispose()` method
    - Update Video.js mock to include `registerComponent()` method

--- tests/mocks/video-js-mock.ts
+++ tests/mocks/video-js-mock.ts
@@ -1,5 +1,6 @@
 import { vi } from 'vitest';
 import type { VideoJs } from '../../@types/videojs';
+import type { VideoJsPlayer } from '../../@types/videojs';
 
 export class MockButton {
    // These properties are used by the tests to verify that the button
@@ -7,13 +8,19 @@ export class MockButton {
    public _controlText = '';
    public _superDisposed = false;
    private _el: HTMLButtonElement;
+   private _player: VideoJsPlayer;
 
-   public constructor() {
+   public constructor(player: VideoJsPlayer) {
+      this._player = player;
       this._el = document.createElement('button');
    }
 
    public el(): HTMLButtonElement {
       return this._el;
+   }
+
+   public player(): VideoJsPlayer {
+      return this._player;
    }
 
    public addClass(className: string): void {
@@ -32,6 +39,10 @@ export class MockButton {
       this._el.classList.add('vjs-hidden');
    }
 
+   public localize(text: string): string {
+      return text;
+   }
+
    public controlText(text: string): void {
       this._controlText = text;
    }
@@ -48,10 +59,16 @@ export class MockButton {
 vi.mock('@silvermine/video.js', () => {
    class MockPlugin {
       public readonly log: ReturnType<typeof vi.fn> & { error: ReturnType<typeof vi.fn> };
+      public readonly player: VideoJsPlayer;
 
-      public constructor() {
+      public constructor(player: VideoJsPlayer) {
          this.log = vi.fn() as MockPlugin['log'];
          this.log.error = vi.fn();
+         this.player = player;
+      }
+
+      public dispose(): void {
+         return;
       }
    }
 
@@ -76,6 +93,7 @@ vi.mock('@silvermine/video.js', () => {
          VERSION: 'test-videojs-version',
          log: vi.fn(),
          registerPlugin: vi.fn(),
+         registerComponent: vi.fn(),
          getPlugin,
          getComponent,
       } as unknown as VideoJs,
